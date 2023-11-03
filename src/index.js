f5b883d754c4929810a939159561de0cfd6ffe2d
Omar Vivas <ovivasar@gmail.com>
2023-11-03 10:13:03 -0500

    login web

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import BienvenidaXpert from './components/BienvenidaXpert'; // Importa el componente de bienvenida
 
-const domain = "dev-pnsmznq2mghesucr.us.auth0.com";
+//const domain = "dev-pnsmznq2mghesucr.us.auth0.com"; //auth0 desde ovivasar
+//const clientId = "tJQf7gtrJtwipbk08LTQ65M0DAjhw9hT"; //auth0 desde ovivasar
+
+
+const domain = "dev-i4ndxhhmhmbyd4tt.us.auth0.com"; //auth0 propio expertcontperu@gmail.com
 
 //Version Local
-const clientId = "tJQf7gtrJtwipbk08LTQ65M0DAjhw9hT";
+//const clientId = "sRVhE9SzcBOGmeDSncrECCLuGA88O8FQ"; //auth0 propio expertcontperu@gmail.com
 
 //Version Web
-//const clientId = "o7uLYv4yImQjxe5jC0djeMKvguaspdnt";
+const clientId = "cVGIEcCJEINGNoLG9khMuXeErdZ3C8jm"; //auth0 propio expertcontperu@gmail.com
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -53,7 +57,8 @@ function Main() {
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          redirect_uri: "http://localhost:3000"
+          //redirect_uri: "http://localhost:3000"
+          redirect_uri: "https://xpertcont-frontend-react-production.up.railway.app/"
         }}
       >
         {/* Renderiza la pantalla de bienvenida o la aplicación principal según el estado */}
