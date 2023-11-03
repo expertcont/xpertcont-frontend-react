fcfa39ecb3a3366440fe6ead75f99ac26fc5789f
Omar Vivas <ovivasar@gmail.com>
2023-11-03 10:23:10 -0500

    inicio

--- src/index.js
+++ src/index.js
@@ -6,37 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 import BienvenidaXpert from './components/BienvenidaXpert'; // Importa el componente de bienvenida
 
-//const domain = "dev-pnsmznq2mghesucr.us.auth0.com"; //auth0 desde ovivasar
-//const clientId = "tJQf7gtrJtwipbk08LTQ65M0DAjhw9hT"; //auth0 desde ovivasar
-
 
 const domain = "dev-i4ndxhhmhmbyd4tt.us.auth0.com"; //auth0 propio expertcontperu@gmail.com
 
 //Version Local
-//const clientId = "sRVhE9SzcBOGmeDSncrECCLuGA88O8FQ"; //auth0 propio expertcontperu@gmail.com
+const clientId = "sRVhE9SzcBOGmeDSncrECCLuGA88O8FQ"; //auth0 propio expertcontperu@gmail.com
 
 //Version Web
-const clientId = "cVGIEcCJEINGNoLG9khMuXeErdZ3C8jm"; //auth0 propio expertcontperu@gmail.com
+//const clientId = "cVGIEcCJEINGNoLG9khMuXeErdZ3C8jm"; //auth0 propio expertcontperu@gmail.com
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-/*
-root.render(
-  <React.StrictMode>
-    <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    //redirectUri="http://localhost:3000"
-    //redirectUri="https://alsasrl.com.pe/carguio"
-
-    authorizationParams={{
-      redirect_uri: "http://localhost:3000"
-    }}
-    >
-    <App />
-    </Auth0Provider>
-  </React.StrictMode>
-);*/
 
 function Main() {
   // Define un estado para controlar si se muestra la pantalla de bienvenida o la aplicación principal.
@@ -57,8 +36,8 @@ function Main() {
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          //redirect_uri: "http://localhost:3000"
-          redirect_uri: "https://xpertcont-frontend-react-production.up.railway.app/"
+          redirect_uri: "http://localhost:3000"
+          //redirect_uri: "https://xpertcont-frontend-react-production.up.railway.app/"
         }}
       >
         {/* Renderiza la pantalla de bienvenida o la aplicación principal según el estado */}
