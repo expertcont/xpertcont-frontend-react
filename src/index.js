748e3f4881e4af0ef69828f63c6e794b3eccccf6
Omar <ovivasar@gmail.com>
2025-11-10 09:16:42 -0500

    export xls

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import BienvenidaXpert from './components/BienvenidaXpert'; // Importa el compon
 const domain = "dev-i4ndxhhmhmbyd4tt.us.auth0.com"; //auth0 propio expertcontperu@gmail.com
 
 //Version Local
-const clientId = "sRVhE9SzcBOGmeDSncrECCLuGA88O8FQ"; //auth0 propio expertcontperu@gmail.com
+//const clientId = "sRVhE9SzcBOGmeDSncrECCLuGA88O8FQ"; //auth0 propio expertcontperu@gmail.com
 
 //Version Web Ralwaiy
 //const clientId = "cVGIEcCJEINGNoLG9khMuXeErdZ3C8jm"; //auth0 propio expertcontperu@gmail.com
 
 //Version Web expertcont.pe
-//const clientId = "vFKcLZ6SHdwEWUWrMzkty0PDVjLbTZrW"; //auth0 propio expertcontperu@gmail.com
+const clientId = "vFKcLZ6SHdwEWUWrMzkty0PDVjLbTZrW"; //auth0 propio expertcontperu@gmail.com
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -39,9 +39,9 @@ function Main() {
         domain={domain}
         clientId={clientId}
         authorizationParams={{
-          redirect_uri: "http://localhost:3000"
+          //redirect_uri: "http://localhost:3000"
           //redirect_uri: "https://expertcont.up.railway.app/"
-          //redirect_uri: "https://expertcont.pe/"
+          redirect_uri: "https://expertcont.pe/"
         }}
       >
         {/* Renderiza la pantalla de bienvenida o la aplicación principal según el estado */}
